e732f1a509d4e7854f852e3d7bf1b78cbddd713e
Melissa <>
2025-02-03 06:20:51 -0800

    Tested successfully using Melissa data files for 2025.Q1

--- MelissaProfilerObjectWindowsPython3.ps1
+++ MelissaProfilerObjectWindowsPython3.ps1
@@ -28,7 +28,7 @@ class ManifestConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2024.Q4'
+$RELEASE_VERSION = '2025.Q1'
 $ProductName = "profiler_data"
 
 # Uses the location of the .ps1 file 
